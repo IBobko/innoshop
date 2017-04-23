21ad3db04d947ce6e28ba4be0b8c4aa97f1fab58
Stepanov Vyacheslav <v.stepanov@innopolis.ru>
2017-04-23 13:15:05 +0300

    added pachages and files

---
+++ src/main/java/ru/innopolis/innoshop/cart/CartModel.java
@@ -0,0 +1,7 @@
+package ru.innopolis.innoshop.cart;
+
+/**
+ * Created by Vyacheslav on 23.04.2017.
+ */
+public class CartModel {
+}
---
+++ src/main/java/ru/innopolis/innoshop/delivery/DeliveryPageController.java
@@ -0,0 +1,7 @@
+package ru.innopolis.innoshop.delivery;
+
+/**
+ * Created by Vyacheslav on 23.04.2017.
+ */
+public class DeliveryPageController {
+}
